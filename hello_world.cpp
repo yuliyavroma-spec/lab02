4f8bccdec3b1f37e3b531df3a17f6489e41c7703
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-17 00:08:40 +0300

    apply clang-format with Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main() {
-std::string name;
-std::cout << "Enter your name: ";
-std::cin >> name;
-std::cout << "Hello world from @" << name << std::endl;
-return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from @" << name << std::endl;
+  return 0;
 }
