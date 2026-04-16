4b3a65f981dc3a10d22edcb6778c080ff080b158
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-17 00:49:37 +0300

    add English commets

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <string>
-int
-main()
-{
-  std::string name;
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  std::cout << "Hello world from @" << name << std::endl;
-  return 0;
+#include <iostream>  // for std::cout, std::cin
+#include <string>    // for std::string
+
+// Main function
+int main() {
+    std::string name;  // variable to store the name
+    std::cout << "Enter your name: ";  // prompt for name
+    std::cin >> name;  // read the name
+    std::cout << "Hello world from @" << name << std::endl;  // print greeting
+    return 0;  // successful completion
 }
