8d1506162c16c25fba66760826512c30bb9aa704
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-16 23:01:03 +0300

    add name input to hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-cout << "Hello world" << endl;
+string name;
+cout << "Enter your name: ";
+cin >> name;
+cout << "Hello world from @" << name << endl;
 return 0;
 }
